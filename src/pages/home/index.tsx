b6c2ff19cca8eab945d775b05a2bfc458f0c61f5
xiangshangzhi <xiangshangzhi@163.com>
2023-07-10 15:44:17 +0800

    feat: init首页banner

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -9,6 +9,7 @@ import { useQuery, useQueryClient } from 'umi'
 import { Button, Space } from 'antd'
 import { userProfileStore } from '@/model/userProfile'
 import { getHomeData } from './service/api'
+import Banner from './components/banner'
 
 const HomePage: React.FC<any> = () => {
   const { data, error, isLoading, refetch } = useQuery(
@@ -26,39 +27,13 @@ const HomePage: React.FC<any> = () => {
   const userProfile = userProfileStore()
   console.log('@@@@@userProfile======', userProfile)
 
-  const changeName = () => {
-    console.log('@@@@@userProfile======', userProfile)
-    const name = userProfile.name
-    userProfile.changeName(name === 'xiaoming' ? 'lorentz' : 'xiaoming')
-  }
-  const isLogin = userProfile.isLogin
-  const changeLoginStatus = () => {
-    if (isLogin) {
-      userProfile.logout()
-    } else {
-      userProfile.login()
-    }
-  }
-
   return (
     <>
       {isLoading ? (
         <div>loading...</div>
       ) : (
         <div>
-          <h1>this is HomePage</h1>
-          <h1>{`home data is ${data}`}</h1>
-          <h1>{`current name is ${userProfile.name}`}</h1>
-          <h1>{`登录状态  ${isLogin ? '已登陆' : '未登录'}`}</h1>
-          <Space>
-            <Button onClick={changeName} type='primary'>
-              change name
-            </Button>
-            <Button onClick={changeLoginStatus} type='primary'>
-              change loginstatus
-            </Button>
-            <Button> request </Button>
-          </Space>
+          <Banner bannerList={[1, 2, 3, 4, 5]} />
         </div>
       )}
     </>
