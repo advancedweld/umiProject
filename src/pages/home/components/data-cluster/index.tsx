54be0210be6630536092cddc31878e08c8f751cd
xiangshangzhi <xiangshangzhi@163.com>
2023-07-11 10:15:39 +0800

    feat: 数据集群

--- src/pages/home/components/data-cluster/index.tsx
+++ src/pages/home/components/data-cluster/index.tsx
@@ -2,14 +2,25 @@ import React from 'react'
 
 import SectionTitle from '../section-title'
 import styles from './index.less'
-import { Space, Card } from 'antd'
+import { Space, Card, Button } from 'antd'
 
 const ContentCard: React.FC = () => (
-  <Card style={{ width: 300 }}>
-    <p>Card content</p>
-    <p>Card content</p>
-    <p>Card content</p>
-  </Card>
+  <div className={styles.cardWrap}>
+    <div
+      style={{ color: 'rgba(0,0,0,0.85)', fontSize: '16px', fontWeight: 500 }}>
+      商业构成库
+    </div>
+    <div
+      style={{
+        color: 'rgba(0,0,0,0.45)',
+        fontSize: '14px',
+        fontWeight: 400,
+        marginTop: '8px',
+      }}>
+      精选优秀数据
+    </div>
+    <Button type='link'>查看详情</Button>
+  </div>
 )
 
 interface IDataCluster {
@@ -21,7 +32,20 @@ const DataCluster: React.FC<IDataCluster> = ({ bannerList }) => {
   return (
     <div className={styles.wrap}>
       <SectionTitle mainTitle='数据集群' subTitle='DATA CLUSTER' />
-      <div className={styles.infoColumn}>
+      <div className={styles.section} style={{ marginTop: '40px' }}>
+        {[1, 2, 3, 4].map((item, index) => {
+          return (
+            <div key={index} className={styles.subTitle}>
+              <div>
+                <span className={styles.number}>20</span>
+                <span className={styles.unit}>个</span>
+              </div>
+              <div className={styles.hints}>核心数据库</div>
+            </div>
+          )
+        })}
+      </div>
+      <div className={styles.section} style={{ marginTop: '32px' }}>
         {[1, 2, 3, 4].map((item, index) => {
           return <ContentCard key={index} />
         })}
