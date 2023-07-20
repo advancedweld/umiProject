644f9d9bb4555b752e12c52d4f98be66caea5e49
xiangshangzhi <xiangshangzhi@163.com>
2023-07-20 10:45:51 +0800

    feat: 完善数据集群卡片

--- src/pages/data-cluster/components/CardList.tsx
+++ src/pages/data-cluster/components/CardList.tsx
@@ -1,19 +1,47 @@
 import React from 'react'
+import { Tag } from 'antd'
+import cx from 'classnames'
+import { ReactComponent as DownloadIcon } from '@/assets/img/download_arrow_icon.svg'
 import { useFilterContext } from '../context'
-
-import LibTabs from './LibTabs'
+import { ReactComponent as ViewIcon } from '@/assets/img/view_icon.svg'
 import styles from './style.less'
 
-const DataClusterCard: React.FC = () => {
-  const { filterState, filterOperations } = useFilterContext()
-  console.log('@@@@@@@@@@@@@@数据集群=====', filterState, filterOperations)
+interface ICardProps {
+  title: string
+  /* 是否是人本构成库 */
+  isHumanType?: boolean
+}
+const DataClusterCard: React.FC<ICardProps> = ({ title, isHumanType }) => {
   return (
     <div className={styles.cardWrap}>
-      <div>卡片卡片卡片卡片</div>
+      <div className={cx(styles.imgContainer, { [styles.mask]: isHumanType })}>
+        <img src='www.google.com' />
+        {!!isHumanType && (
+          <div className={styles.downloadIcon}>
+            <DownloadIcon />
+          </div>
+        )}
+      </div>
+
+      <div className={styles.title}>{title}</div>
+      <div className={styles.desc}>
+        <div className={styles.tagContainer}>
+          <Tag>宫廷风</Tag>
+          <Tag>人体生理信号</Tag>
+          <Tag>宫廷风</Tag>
+        </div>
+
+        <div className={styles.viewCount}>
+          <ViewIcon style={{ marginRight: '4px' }} />
+          <span>66</span>
+        </div>
+      </div>
     </div>
   )
 }
-
+interface ICardListProps {
+  cardList: any[]
+}
 const CardList: React.FC = () => {
   const { filterState, filterOperations } = useFilterContext()
   console.log('@@@@@@@@@@@@@@数据集群=====', filterState, filterOperations)
@@ -21,7 +49,7 @@ const CardList: React.FC = () => {
     <div className={styles.cardListWrap}>
       <div className={styles.scrollWrap}>
         {new Array(10).fill(0).map((item, index) => (
-          <DataClusterCard key={index} />
+          <DataClusterCard key={index} title='我是一张小卡片' isHumanType={index % 2 === 0} />
         ))}
       </div>
     </div>
