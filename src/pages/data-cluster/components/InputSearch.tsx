b4f43e286a8539ef954504ef005e0497b42e59b1
xiangshangzhi <xiangshangzhi@163.com>
2023-07-21 08:59:02 +0800

    feat: 完善数据集群页选项

--- src/pages/data-cluster/components/InputSearch.tsx
+++ src/pages/data-cluster/components/InputSearch.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Children } from 'react'
 import { Space, Input } from 'antd'
 import cx from 'classnames'
 import { useFilterContext } from '../context'
@@ -7,17 +7,90 @@ import { TAB_ITEMS_OBJECT } from '../service/constants'
 import { ReactComponent as SearchIcon } from '@/assets/img/search_icon.svg'
 import styles from './style.less'
 
+const TABS_META = {
+  ALL_LIB: [],
+  ART_LIB: [],
+
+  BUSINESS_LIB: [
+    {
+      label: '全部',
+      value: 'ALL',
+      children: [],
+    },
+    {
+      label: '纽扣',
+      value: 'NIUKOU',
+      children: [],
+    },
+    {
+      label: '服装',
+      value: 'CLOTHES',
+      children: [],
+    },
+  ],
+  TECH_LIB: [],
+  CLUTURE_LIB: [],
+  DESIGN_LIB: [
+    {
+      label: '全部',
+      value: 'ALL',
+      children: [],
+    },
+    {
+      label: '设计风格',
+      value: 'designStyle',
+      children: [
+        {
+          label: '包豪斯',
+          value: 'bhs',
+        },
+        {
+          label: '后现代',
+          value: 'hxd',
+        },
+      ],
+    },
+    {
+      label: '设计师',
+      value: 'designer',
+      children: [
+        {
+          label: '向尚志',
+          value: 'xsz',
+        },
+        {
+          label: '宋志勤',
+          value: 'szq',
+        },
+      ],
+    },
+  ],
+
+  HUMAN_LIB: [],
+  FUSION_LIB: [],
+}
 const LibTabs: React.FC = () => {
   const { filterState, filterOperations } = useFilterContext()
 
   const [searchValue, setSearchValue] = React.useState(filterState.searchKeyWord)
   /* 当前激活的tab */
   const activeTab = filterState.libTab
+  /* 当前激活的二级tab */
+  const activeTab2 = filterState.secondTab
+  /* 当前选项元数据 */
+  const selections2 = TABS_META[activeTab] || []
+  console.log('@@@@@selections2=====', selections2)
 
+  const selections3 = selections2.find((item) => item.value === activeTab2)?.children || []
+
+  console.log('@@@@@selections3=====', selections3)
   const handleSeacrh = () => {
     filterOperations.updateSearchKeyWord(searchValue)
   }
-
+  const handleTab2click = (tab: string) => {
+    console.log('@@@@@handleTab2click=====', tab)
+    filterOperations.updateTabs2(tab)
+  }
   const title = activeTab === 'ALL_LIB' ? '数据集群' : TAB_ITEMS_OBJECT[activeTab]
   return (
     <div className={styles.searchWrap}>
@@ -35,15 +108,35 @@ const LibTabs: React.FC = () => {
         className={styles.searchInput}
         suffix={<SearchIcon style={{ cursor: 'pointer' }} onClick={handleSeacrh} />}></Input>
 
-      <div className={styles.columnWrap} style={{ marginTop: '24px' }}>
-        <div className={styles.tab2}>全部</div>
-        <div className={styles.tab2}>设计风格</div>
-      </div>
-      <div className={styles.columnWrap} style={{ marginTop: '12px' }}>
+      {!!selections2.length && (
+        <div className={styles.columnWrap} style={{ marginTop: '24px' }}>
+          {selections2.map((item) => {
+            return (
+              <div className={styles.tab2} key={item.value} onClick={() => handleTab2click(item.value)}>
+                {item.label}
+              </div>
+            )
+          })}
+        </div>
+      )}
+
+      {!!selections3.length && (
+        <div className={styles.columnWrap} style={{ marginTop: '12px' }}>
+          {selections3.map((item) => {
+            return (
+              <div className={styles.tab3} key={item.value}>
+                {item.label}
+              </div>
+            )
+          })}
+        </div>
+      )}
+
+      {/* <div className={styles.columnWrap} style={{ marginTop: '12px' }}>
         <div className={styles.tab3}>包豪斯</div>
         <div className={styles.tab3}>复古风</div>
         <div className={styles.tab3}>后现代</div>
-      </div>
+      </div> */}
     </div>
   )
 }
--- src/pages/data-cluster/context.tsx
+++ src/pages/data-cluster/context.tsx
@@ -4,7 +4,7 @@ type PageParams = {
   pageNum: number
   pageSize: number
 }
-type ActionType = 'UPDATE_PAGE_PARAMS' | 'UPDATE_LIB_TABS' | 'UPDATE_SEARCH_KEYWORD'
+type ActionType = 'UPDATE_PAGE_PARAMS' | 'UPDATE_LIB_TABS' | 'UPDATE_TABS_2' | 'UPDATE_TABS_3' | 'UPDATE_SEARCH_KEYWORD'
 
 type LibTab = keyof typeof TAB_ITEMS_OBJECT
 
@@ -12,12 +12,18 @@ type State = {
   searchKeyWord: string
   pageParams: PageParams
   libTab: LibTab
+  /* 二级分类 */
+  secondTab: string
+  /* 三级分类 */
+  thirdTab: string
 }
 type Action = { type: ActionType; payload: any }
 type FilterOperations = {
   updatePageParams: (payload: PageParams) => void
   updateLibTabs: (payload: LibTab) => void
   updateSearchKeyWord: (payload: string) => void
+  updateTabs2: (payload: string) => void
+  updateTabs3: (payload: string) => void
 }
 
 type FilterContextType = {
@@ -33,6 +39,8 @@ const useCreateFilterContext = () => {
       pageSize: 10,
     },
     libTab: 'ALL_LIB',
+    secondTab: '',
+    thirdTab: '',
   }
 
   const reducer = (state: State, action: Action): State => {
@@ -46,6 +54,18 @@ const useCreateFilterContext = () => {
         return {
           ...state,
           libTab: action.payload,
+        }
+
+      case 'UPDATE_TABS_2':
+        return {
+          ...state,
+          secondTab: action.payload,
+        }
+
+      case 'UPDATE_TABS_3':
+        return {
+          ...state,
+          thirdTab: action.payload,
         }
       /* 关键词搜索 */
       case 'UPDATE_SEARCH_KEYWORD':
@@ -67,11 +87,24 @@ const useCreateFilterContext = () => {
     dispatch({ type: 'UPDATE_LIB_TABS', payload })
   }
 
+  const updateTabs2 = (payload: string) => {
+    dispatch({ type: 'UPDATE_TABS_2', payload })
+  }
+
+  const updateTabs3 = (payload: string) => {
+    dispatch({ type: 'UPDATE_TABS_3', payload })
+  }
   const updateSearchKeyWord = (payload: string) => {
     dispatch({ type: 'UPDATE_SEARCH_KEYWORD', payload })
   }
 
-  const filterOperations: FilterOperations = { updatePageParams, updateLibTabs, updateSearchKeyWord }
+  const filterOperations: FilterOperations = {
+    updatePageParams,
+    updateLibTabs,
+    updateSearchKeyWord,
+    updateTabs2,
+    updateTabs3,
+  }
 
   return {
     filterState,
